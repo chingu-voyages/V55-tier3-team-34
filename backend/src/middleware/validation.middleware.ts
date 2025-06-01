3a0b27cd5f7de5d6d819c93439876d8b7498b884
TiaDev7474 <nomenjanaharyriry@gmail.com>
2025-06-01 19:42:11 +0300

    fix validation error

--- backend/src/middleware/validation.middleware.ts
+++ backend/src/middleware/validation.middleware.ts
@@ -1,18 +1,23 @@
-import {ZodError , ZodObject , ZodRawShape , ZodTypeAny} from "zod";
+
 import {NextFunction , Request , Response} from "express";
+import {ZodSchema , ZodError } from "zod";
 
 
-export function validateData(schema: ZodObject<ZodTypeAny>) {
+function isZodError(e: any): e is ZodError {
+    return typeof e === "object" && e?.name === "ZodError" && Array.isArray(e.issues);
+}
+
+export function validateData(schema: ZodSchema) {
     return (req: Request, res: Response, next: NextFunction) => {
         try {
             req.body = schema.parse(req.body);
             next();
-        } catch (e) {
-            if (e instanceof ZodError) {
-                const errorMessages = e.errors.map((issue) => ({
+        }  catch (e: any) {
+            if (isZodError(e)) {
+                const errorMessages = e.issues.map((issue) => ({
                     message: `${issue.path.join(".")} is ${issue.message}`,
                 }));
-                res.status(400).json({ error: "Invalid Data", details: errorMessages });
+                return res.status(400).json({ error: "Invalid Data", details: errorMessages });
             } else {
                 res.status(500).json({ error: "Internal Server Error" });
             }
